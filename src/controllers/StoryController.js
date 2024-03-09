33c89a921f2c5d70ec1a603c26630db0071c6794
Benyapa Khiaotai <benyapa_kh@cmu.ac.th>
2024-03-09 10:19:28 +0700

    agreegation

--- src/controllers/StoryController.js
+++ src/controllers/StoryController.js
@@ -2,99 +2,107 @@ const Storys = require('../models/StoryModel')
 const User = require('../models/UsersModel')
 const Episode = require('../models/EpisodeModel')
 
-const getStorys = async(request,reply) => {
-    try{
+const getStorys = async (request, reply) => {
+    try {
         const stories = await Storys.find()
         reply.send(stories)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
-const getStoryByID = async(request,reply) => {
-    try{
+const getStoryByID = async (request, reply) => {
+    try {
         const story = await Storys.findById(request.params.id)
         reply.send(story)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
-const getStoryByName = async(request,reply) => {
-    try{
-        var query = {
-            ownerId: request.params.id
-        };
-        const story = await Storys.find(query)
+const getStoryByName = async (request, reply) => {
+    try {
+        var query = [
+            { $match: { ownerId: request.params.id } },
+            {
+                $lookup: {
+                    from: "episodes",
+                    localField: "_id",
+                    foreignField: "StoryId",
+                    as: "episodesIn"
+                }
+            }
+        ]
+        const story = await Storys.aggregate(query)
         reply.send(story)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
-const getStoryByCategory = async(request,reply) => {
-    try{
+const getStoryByCategory = async (request, reply) => {
+    try {
         var query = {
             category: request.params.id,
             IsPublic: true
         };
         const story = await Storys.find(query)
         reply.send(story)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
-const postStory = async(request,reply) => {
-    try{
-        
-        if(request.body.EpisodeId != null){
-            for(let epId of request.body.EpisodeId){
+const postStory = async (request, reply) => {
+    try {
+
+        if (request.body.EpisodeId != null) {
+            for (let epId of request.body.EpisodeId) {
                 const episode = await Episode.findById(epId)
-                if(!episode){
-                    return reply.status(400).send({message: `Invalid episode: ${epId}`})
+                if (!episode) {
+                    return reply.status(400).send({ message: `Invalid episode: ${epId}` })
                 }
             }
         }
-        const story = new Storys(request.body) 
+        const story = new Storys(request.body)
         const result = story.save()
         reply.send(story)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
-const putStory = async(request,reply) => {
-    try{
-        const story = await Storys.findByIdAndUpdate(request.params.id,request.body)
+const putStory = async (request, reply) => {
+    try {
+        const story = await Storys.findByIdAndUpdate(request.params.id, request.body)
         reply.send(story)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
-const deleteStory = async(request,reply) => {
-    try{
+const deleteStory = async (request, reply) => {
+    try {
         const story = await Storys.findByIdAndDelete(request.params.id)
         reply.status(203).send(story)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
-const searchStory = async(request,reply) => {
-    try{
+const searchStory = async (request, reply) => {
+    try {
         var query = {
             IsPublic: true,
-            "$or":[
-                {storyname: {$regex:request.params.id}}
+            "$or": [
+                { storyname: { $regex: request.params.id } }
             ]
         };
         const story = await Storys.find(query)
         reply.send(story)
-    }catch (error){
+    } catch (error) {
         reply.status(500).send(error)
-    }   
+    }
 }
 
 
